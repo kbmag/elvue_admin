7474e6710867b47c7c9cff8a1b9fac6033b96dd8
1055277326@qq.com <1055277326@qq.com>
2020-06-15 22:38:13 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
 import './assets/resrt.css'
 
 // 导入ElementUI
 import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';//样式文件一定要引入
 
 // 使用ElementUI
 Vue.use(ElementUI);
